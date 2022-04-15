f7cc6555297852707b163e92e2e263f4fa408f96
Christian8Bit <55551092+Christian8Bit@users.noreply.github.com>
2022-04-16 00:21:37 +0800

    Update App.java

--- CourseworkTwo/src/App.java
+++ CourseworkTwo/src/App.java
@@ -1,7 +1,4 @@
-/*
-Student Name: Bonifacio Christian Bautista
-Student Registration number: 190462509
-*/
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
